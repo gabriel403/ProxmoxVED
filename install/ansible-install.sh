db722525268d17f79ede82acac3ea6d880b3b598
Gabriel Baker <me@g403.co>
2026-05-21 19:59:31 +0100

    Add Ansible controller LXC script

---
+++ install/ansible-install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: Gabriel Baker (gbaker403)
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://github.com/ansible/ansible
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Ansible"
+$STD apt install -y \
+  ansible \
+  git
+msg_ok "Installed Ansible"
+
+msg_info "Installing community.general Collection"
+$STD ansible-galaxy collection install community.general
+msg_ok "Installed community.general Collection"
+
+msg_info "Generating SSH Keypair"
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+ssh-keygen -t ed25519 -f /root/.ssh/id_ed25519 -N "" -C "ansible-controller" >/dev/null 2>&1
+chmod 600 /root/.ssh/id_ed25519
+msg_ok "Generated SSH Keypair"
+
+motd_ssh
+customize
+cleanup_lxc
